20f30ad53be802823a606cc367c25102da3a2ffa
Donnie Ellstrom <donnie.ellstrom@freshconsulting.com>
2018-02-24 17:46:08 -0800

    Scaffold project via Angular CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  reactiveForm: FormGroup;
+  name: String = '';
+  description: String = '';
+  submit: any;
+
+
+  constructor(private fb: FormBuilder) {
+
+    this.reactiveForm = this.fb.group({
+      // formControlName: [value, validator]
+      name: [null, Validators.required],
+      description: [null, Validators.compose([Validators.required, Validators.max(50)])],
+      validate: '',
+    });
+
+  }
+
+  // Get our form info on submission
+  handleFormSubmit(submit) {
+    this.description = submit.description;
+    this.name = submit.name;
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+// Validators
+// min(min: number)
+// max(max: number)
+// required(control: AbstractControl)
+// requiredTrue(control: AbstractControl)
+// email(control: AbstractControl)
+// minLength(minLength: number)
+// maxLength(maxLength: number)
+// pattern(pattern: string | RegExp)
+// nullValidator(c: AbstractControl)
+// compose(validators: | null | undefined)
+// composeAsync(validators: (Asy | null)[])
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule,
+} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
