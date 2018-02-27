45c31f2f581ddc210d3e1702f17eb9463873e7d5
Donnie Ellstrom <donnie.ellstrom@freshconsulting.com>
2018-02-27 13:29:16 -0800

    Add more form template functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,57 +8,84 @@ import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
 })
 export class AppComponent implements OnInit, OnChanges, AfterViewInit {
 
-  reactiveForm: FormGroup;
-  userName: String = '';
-  userNameValidation: String = 'This field is required';
-  userInfo: String = '';
-  displayForm: any;
+  // Template form
+  templateDisabled: String = '';
   templateUserName: String = '';
-  templateUserDescription: String = '';
+  templateUserInfo: String = '';
+  templateFormControls: any;
   @ViewChild(NgForm) templateForm: NgForm;
 
+  // Reactive form
+  reactiveForm: FormGroup;
+  reactiveFormControls: any;
+  userNameValidationWarning: String = 'This field is required';
+  userInfoValidationWarning: String = 'This field must be at least 10 characters long';
 
   constructor(private fb: FormBuilder) {
-
     this.reactiveForm = this.fb.group({
-      // formControluserName: [value, validator]
+      // Disabled state is improperly set in the template (console throws warning)
+      improperDisabled: [null],
+      // Disabled is properly set on the FormControl
+      properDisabled: [ { value: null, disabled: true } ],
       userName: [null, Validators.required],
       // for multiple validators use Validators.compose
-      userInfo: [null, Validators.compose([Validators.required, Validators.min(10), Validators.max(50)])],
-      toggleValidate: '',
+      userInfo: [null, Validators.compose([Validators.required, Validators.minLength(10)])],
+      toggleNameValidation: [false],
+      toggleInfoValidation: [false]
     });
   }
 
   ngOnInit() {
-    this.reactiveForm.controls.toggleValidate.valueChanges.subscribe(change => {
-      console.log('change: ', change);
+    this.templateFormControls = Object.keys(this.templateForm.controls);
+    this.reactiveFormControls = Object.keys(this.reactiveForm.controls);
 
-      if (change === true) {
-        console.log('here');
-        // When adding validation to the current validators array you have to include all values (previous and new)
-        this.reactiveForm.get('userName').setValidators([Validators.required, Validators.minLength(10)]);
-        this.userNameValidation = 'First & Last name required';
-        console.log(this.reactiveForm.controls.userName);
-      } else {
-        this.reactiveForm.get('userName').setValidators([Validators.required]);
-      }
-      // After changing validators you have to update the form and validity state
-      this.reactiveForm.get('userName').updateValueAndValidity();
+    this.reactiveForm.get('toggleNameValidation').valueChanges.subscribe(change => {
+        this.toggleNameValidation();
+    });
+
+    this.reactiveForm.get('toggleInfoValidation').valueChanges.subscribe(change => {
+      this.toggleInfoValidation();
     });
   }
 
   ngAfterViewInit() {
-    console.log('reactive: ', this.reactiveForm);
-    console.log('template: ', this.templateForm);
-    console.log('template controls: ', this.templateForm.controls);
+    console.log('TEMPLATE form controls AfterViewInit: ', this.templateForm.controls);
+    console.log('REACTIVE form controls AfterViewInit: ', this.reactiveForm.controls);
   }
 
-  ngOnChanges() {
-    console.log('template: ', this.templateForm.controls);
+  ngOnChanges() {}
+
+  toggleNameValidation() {
+    if (this.reactiveForm.get('toggleNameValidation').value === true) {
+      // When adding validation to the current validators array you have to include all values (previous and new)
+      this.reactiveForm.get('userName').setValidators([Validators.required, Validators.minLength(10)]);
+    } else {
+      this.reactiveForm.get('userName').setValidators([Validators.required]);
+      // Reset validation warning to original value to match state of validators
+      this.userNameValidationWarning = 'This field is required';
+    }
   }
 
-  showChanges() {
-    console.log('template: ', this.templateForm.controls);
+  toggleInfoValidation() {
+    if (this.reactiveForm.get('toggleInfoValidation').value === true) {
+      this.reactiveForm.get('userName').setValidators([Validators.required, Validators.minLength(10), Validators.maxLength(50)]);
+      this.userInfoValidationWarning = 'This field must be between 10 and 50 characters long';
+    } else {
+      this.reactiveForm.get('userName').setValidators([Validators.required, Validators.minLength(10)]);
+      this.userInfoValidationWarning = 'This field must be at least 10 characters long';
+    }
+    this.reactiveForm.get('userInfo').updateValueAndValidity();
+  }
+
+  UpdateTemplateForm() {
+    this.templateFormControls = Object.keys(this.templateForm.controls);
+  }
+
+  // Demonstrate the necessity for updateValueAndValidity
+  updateReactiveFormUserName() {
+    this.reactiveForm.get('userName').updateValueAndValidity();
+    // Update validation warning to match updated state of validators
+    this.userNameValidationWarning = 'First and last name required';
   }
 
 }
